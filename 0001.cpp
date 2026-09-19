1f5cfdf3733263f9077215bbb1c8765332039a59
Viral Patni <PATNIVIRAL554@GMAIL.COM>
2026-09-19 17:11:45 +0530

    Add 1. Two Sum

---
+++ 0001.cpp
@@ -0,0 +1,22 @@
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+// Language: cpp
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
