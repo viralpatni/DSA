5c9e628c729f6373bc23e32562329c54919df94a
Viral Patni <PATNIVIRAL554@GMAIL.COM>
2026-09-19 18:26:47 +0530

    Add 27. Remove Element

---
+++ 0027.cpp
@@ -0,0 +1,17 @@
+// 27. Remove Element
+// https://leetcode.com/problems/remove-element/
+// Language: cpp
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
